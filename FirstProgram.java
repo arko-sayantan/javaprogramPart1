9f87cb7fc6bc1cb68e1196d5bb032df558da7d73
Sayantan Das <63104414+arko-sayantan@users.noreply.github.com>
2020-07-14 19:02:18 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,16 @@
+public class FirstProgram {
+	public static void main(String args[]) {  //this is the method in other language this is called function.
+												// When a function is inside in a class is called method.
+												// We use static because in order to called main,you have to 
+												// crate object but at starting of programming their is no object so 
+												// thats why we use static word.So if we use static we does not required any object to called a method  
+		// So this is the main method or main function .GUI = Graphical User Interface.
+		System.out.println("Hellow World!");    
+		double num1 = 5.5;
+		double num2 = 6.5;
+		double sum = num1+num2;
+		System.out.println("The result is : "+ sum);
+		char c = 65; // Here we assign integer data type 65 in character but when we run this program we get A
+		System.out.println(c++);// A is the ASCII value of 65.
+	}
+}
